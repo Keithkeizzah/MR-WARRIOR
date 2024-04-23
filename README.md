96e3bb0652ffa6cc39d683f94afde90dadc0df3b
keith <157195350+Keithkeizzah@users.noreply.github.com>
2024-04-23 20:00:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Get Zokou
+
+1. Click on **[Fork](https://github.com/CYBER-WARRIIOR/The-CYBER-MD/fork)** to copy the repo to your GitHub account. Make sure to add a star 🌟 to encourage the developers.
+
+2. Obtain a bot session: 
+
+   [Session-1](https://zokouscan-xbym.onrender.com) ; <br>
+
+
+3. Deploy
+   - **Heroku Deployment:**
+     1. If you don't have a **Heroku** account, click [**here**](https://id.heroku.com/login) to create one.
+     2. Click [**here**](https://dashboard.heroku.com/new?template=https://github.com/CYBER-WARRIIOR/The-CYBER-MD) to deploy the bot on **Heroku**.
+
+## 
---
+++ commandes/menu.js
@@ -0,0 +1,94 @@
+const util = require('util');
+const fs = require('fs-extra');
+const { zokou } = require(__dirname + "/../framework/zokou");
+const { format } = require(__dirname + "/../framework/mesfonctions");
+const os = require("os");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+
+zokou({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    let { ms, repondre ,prefixe,nomAuteurMessage,mybotpic} = commandeOptions;
+    let { cm } = require(__dirname + "/../framework//zokou");
+    var coms = {};
+    var mode = "public";
+    
+    if ((s.MODE).toLocaleLowerCase() != "yes") {
+        mode = "private";
+    }
+
+
+    
+
+    cm.map(async (com, index) => {
+        if (!coms[com.categorie])
+            coms[com.categorie] = [];
+        coms[com.categorie].push(com.nomCom);
+    });
+
+    moment.tz.setDefault('Etc/GMT');
+
+// Créer une date et une heure en GMT
+const temps = moment().format('HH:mm:ss');
+const date = moment().format('DD/MM/YYYY');
+
+  let infoMsg =  `
+╭━༻𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹༺⊰━─
+│   *Préfix* : ${s.PREFIXE}
+│   *Owner* : ${s.OWNER_NAME}
+│   *Mode* : ${mode}
+│   *Commands* : ${cm.length}
+│   *Date* : ${date}
+│   *Hour* : ${temps}
+│   *Développer* : 𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹
+╰─────✧WA-BOT✧─────◆ \n\n`;
+    
+let menuMsg = `
+👋 Hello ${nomAuteurMessage} 👋
+
+*List of commands :*
+◇                             ◇
+`;
+
+    for (const cat in coms) {
+        menuMsg += `╭━༻ ${cat} ༺⊰━─`;
+        for (const cmd of coms[cat]) {
+            menuMsg += `
+✫ ${cmd}`;
+        }
+        menuMsg += `
+╰─────✧WA-BOT✧─────◆ \n`
+    }
+
+    menuMsg += `
+╭━༻𝑪𝒀𝑩𝑬𝑹_𝑾𝑨𝑹𝑹𝑰𝑶𝑹༺⊰━─
+🌸🤭- " 𝐀 𝐅ɑkə 𝐒mıɭə 𝐂ɑη ┣𝐥ıdə  𝐌ıɭɭıoηs 𝐎f 𝐓əɑrs ||%❤😚
+`;
+
+   var lien = mybotpic();
+
+   if (lien.match(/\.(mp4|gif)$/i)) {
+    try {
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" , gifPlayback : true }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+// Vérification pour .jpeg ou .png
+else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+    try {
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Zokou-MD*, développé par Djalega++" }, { quoted: ms });
+    }
+    catch (e) {
+        console.log("🥵🥵 Menu erreur " + e);
+        repondre("🥵🥵 Menu erreur " + e);
+    }
+} 
+else {
+    
+    repondre(infoMsg + menuMsg);
+    
+}
+
+});
